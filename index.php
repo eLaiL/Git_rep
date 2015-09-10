efbd9ada6a51451f76bb6eefbc1e65d168aa3daa
eLaiL <apolon373@gmail.com>
2015-09-10 14:49:41 +0300

    In developing

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
  * Date: 06.09.2015
  * Time: 15:43
  */
-
 function formats ($type){
     $formats = array(
         ".pdf"          =>      "application/pdf",
@@ -76,23 +75,17 @@ function formats ($type){
         ".otf"          =>      "application/vnd.oasis.opendocument.formula-template",
         ".oti"          =>      "application/vnd.oasis.opendocument.image-template",
         ".oth"          =>      "application/vnd.oasis.opendocument.text-web",
-
         # make the default mime type application/octet-stream.
         "."              =>      "text/html",
         ".php"          =>      "php"
     );
-
     foreach ($formats as $k => $v){
         if ($k === $type) return $v;
     }
 }
-
-
-
 function Replacing_key_names($head){
     $mass = explode("\n",$head);
     $http = array_shift($mass);
-
     foreach ($mass as $k => $v){
         $pos = strpos($v,":");
         $sub1 = substr($v,0,$pos);
@@ -103,26 +96,18 @@ function Replacing_key_names($head){
     $mass["http"] = $http;
     return $mass;
 }
-
-
 function sub_input ($head,$ot,$do){
     $strlen = strlen($ot);
     $pos_ot = strpos($head,$ot)+$strlen;
-
     $pos_do = strpos($head,$do,$pos_ot);
     $lenght = $pos_do-$pos_ot;
     $sub = substr($head,$pos_ot,$lenght);
     return $sub;
 }
-
-
-
-
 $sock = socket_create(AF_INET,SOCK_STREAM,0);
 socket_bind($sock,'127.0.0.1',80);
 socket_listen($sock,10);
 socket_set_nonblock($sock);
-
 
 
 $http['connects'] = array ();
@@ -132,71 +117,51 @@ $http['size_lefts'] = array();
 $http['dataToconnects'] = array();
 $http['posInPoint'] = array();
 $http['DownloadSpeed'] = array();
-
 $files = "C:\Users\Я\Desktop\server_eLaiL\htdocs\\";
 
 $con_i = 0;
 $pointer = 0;
 
-
-
-
 while(true) {
     $present = null;
-
     $connect = @socket_accept($sock);//Проверяем новое подключение
     if ($connect !== false) {
-
         $input_data = @socket_read($connect, 1024);
         if ($input_data !== false) {
 
             $filename = @sub_input($input_data, "GET /", " ");
             $ip_xosta = @sub_input($input_data, "Referer: ", "\n");
-
-
             $data_paketa = $ip_xosta . "," . $filename;
-
-
             $dirrr = $files . $filename;
             $exist = file_exists($dirrr);
             if ($exist !== false) {
 
-
                 $kays_datu = array_keys($http['dataToconnects']);
                 foreach ($kays_datu as $k => $value) {
                     $mass_paketov = $http['dataToconnects'][$value];
-
                     if ($data_paketa === $mass_paketov) {
                         $http['connects'][$value][] = $connect;
                         $present = true;
                     }
                 }
-
-
                 if ($present === false or $present === null) {//Если такого нету
                     $http['connects'][$con_i][] = $connect;
                     $http['dataToconnects'][$con_i] = $data_paketa;
                     $http['posInPoint'][$con_i] = 0;
-
                     if ($filename === '1.mp3'){
                         $http['DownloadSpeed'][$con_i] = 1024;
                     }else{
                         $http['DownloadSpeed'][$con_i] = 1024*2;
                     }
-
                 }
-
-
                 $filesize = filesize($dirrr);
                 $pos = strpos($filename, '.');
                 $sub = substr($filename, $pos, strlen($filename));
                 $type = formats($sub);
 
-
                 if ($present !== null) {
                     $http['fopens'][$value][] = fopen($dirrr, 'r');
                     $http['types'][$value][] = $type;
-
                     $mass_replaced_kays = Replacing_key_names($input_data);
 
                     if (isset($mass_replaced_kays['Range'])) {
@@ -205,7 +170,6 @@ while(true) {
                         $len = $pos2 - $pos;
                         $range_bytes = substr($mass_replaced_kays['Range'], $pos + 1, $len);
                         $filesize = $filesize - $range_bytes;
-
                         $http['size_lefts'][$value][] = $filesize;
                         $count = count($http['size_lefts'][$value]);
                         fseek($http['fopens'][$value][$count], $range_bytes);
@@ -215,12 +179,9 @@ while(true) {
                     $http['fopens'][$con_i][] = fopen($dirrr, 'r');
                     $http['types'][$con_i][] = $type;
                 }
-
-
             } else {
                 $error_file = $file . 'error_404.html';
                 $error_size = filesize($error_file);
-
                 date_default_timezone_set('Europe/Kiev');
                 $time = date('r');
                 $scrypt = 'HTTP/1.1 404 Not Found' . "\r\n";
@@ -231,12 +192,12 @@ while(true) {
                 $scrypt .= 'Connection: close' . "\r\n";
                 $scrypt .= 'Content-Type: text/html' . "\r\n";
                 $scrypt .= "\r\n";
-
                 $scrypt .= file_get_contents($error_file);
                 socket_write($connect, $scrypt);
                 socket_close($connect);
             }
             //inpute data file browser
+
             date_default_timezone_set('Europe/Kiev');
             $time  = date('r');
             $scrypt = 'HTTP/1.1 200 OK'."\r\n";
@@ -248,10 +209,8 @@ while(true) {
             $scrypt .= 'Connection: Keep-Alive'."\r\n";
             $scrypt .= 'Content-Type: '.$type."\r\n";
             $scrypt .= "\r\n";
-
             socket_write($connect,$scrypt);
             echo 'connection '.$con_i." start download $dirrr.\r\n";
-
             $con_i++;
         }
     }
@@ -259,20 +218,17 @@ while(true) {
     if (isset($http['connects'][$pointer])){
         $input_data = @socket_read($http['connects'][$pointer][$http['posInPoint'][$pointer]], 1024);
         $err = socket_last_error();
-
         if ($err === 10054) { // Если соединение прервано
             socket_close($http['connects'][$pointer][$http['posInPoint'][$pointer]]);
             unset($http['connects'][$pointer][$http['posInPoint'][$pointer]], $http['fopens'][$pointer][$http['posInPoint'][$pointer]], $http['types'][$pointer][$http['posInPoint'][$pointer]],$http['posInPoint'][$pointer]);
             echo 'connection ' . " closed.\r\n";
         }
-
         if (isset($http['size_lefts'][$pointer][$http['posInPoint'][$pointer]]) and $http['size_lefts'][$pointer][$http['posInPoint'][$pointer]] <= 0) {
             if (isset($http['fopens'][$pointer][$http['posInPoint'][$pointer]])) fclose($http['fopens'][$pointer][$http['posInPoint'][$pointer]]);
             unset($http['posInPoint'][$pointer],$http['fopens'][$pointer][$http['posInPoint'][$pointer]], $http['types'][$pointer][$http['posInPoint'][$pointer]], $http['size_lefts'][$pointer][$http['posInPoint'][$pointer]]);
             echo 'filesend end ' . "\r\n";
         }
     }
-
     if (isset($http['types'][$pointer])) {
         if ($http['types'][$pointer][$http['posInPoint'][$pointer]] === 'php') {
             ob_start();
@@ -282,11 +238,8 @@ while(true) {
             @socket_write($http['connects'][$pointer][$http['posInPoint'][$pointer]], $out1);
         }
     }
-
-
     if (isset($http['types'][$pointer])) {
         if ($http['types'][$pointer][$http['posInPoint'][$pointer]] === 'text/plain' or $http['types'][$pointer][$http['posInPoint'][$pointer]] === 'audio/mpeg') {
-
             $fred = fread($http['fopens'][$pointer][$http['posInPoint'][$pointer]], $http['DownloadSpeed'][$pointer]);
             $bytes = @socket_write($http['connects'][$pointer][$http['posInPoint'][$pointer]], $fred, $http['DownloadSpeed'][$pointer]);
 
@@ -314,13 +267,14 @@ while(true) {
             if ($http['posInPoint'][$pointer] >= count($in_kays)) {
                 $http['posInPoint'][$pointer] = $in_kays[0];
             } else {
-
                 $http['posInPoint'][$pointer] = $in_kays[$http['posInPoint'][$pointer]];
             }
         }
     }
+
     if(!isset($http['fopens'][$pointer])){
         usleep(500000);
     }
 }
 ?>
+
